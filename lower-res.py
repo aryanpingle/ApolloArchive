73b026c3c355c5b8959cdcb3af837f52762c8d7b
robo-mop <realaryanpingle@gmail.com>
2021-08-28 12:56:40 +0530

    Changed lowe-res.py

--- lower-res.py
+++ lower-res.py
@@ -1,10 +1,12 @@
 from PIL import Image
 
+perc = 5
+
 for i in range(1, 5):
     url = f"Images/Title Screen {i}.jpg"
     img = Image.open(url)
     w,h = img.width, img.height
 
-    img = img.resize((w//5, h//5))
+    img = img.resize((int(w * perc / 100), int(h * perc / 100)))
 
     img.save(url.replace(".", " - Low Res."))
