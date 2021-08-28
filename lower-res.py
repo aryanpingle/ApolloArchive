d4d092ffe046daabd39e0699f0e8417d4b949693
robo-mop <realaryanpingle@gmail.com>
2021-08-28 12:51:38 +0530

    Reduced size of low res backgrounds to 1/5th

--- lower-res.py
+++ lower-res.py
@@ -1,9 +1,10 @@
 from PIL import Image
 
-url = "Images/Title Screen 4.jpg"
-img = Image.open(url)
-w,h = img.width, img.height
+for i in range(1, 5):
+    url = f"Images/Title Screen {i}.jpg"
+    img = Image.open(url)
+    w,h = img.width, img.height
 
-img = img.resize((w//4, h//4))
+    img = img.resize((w//5, h//5))
 
-img.save(url.replace(".", " - Low Res."))
+    img.save(url.replace(".", " - Low Res."))
