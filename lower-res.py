3c142de478cb94188178a4d01fec30bf9281f4e9
robo-mop <realaryanpingle@gmail.com>
2021-09-08 16:44:11 +0530

    Deleted deprecated files

--- lower-res.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-img = Image.open("Images/favicon.png")
-
-img = img.resize((64, 64))
-
-img.save("Images/favicon.ico", "ICO")
--- main.py
+++
@@ -1,3 +0,0 @@
-import re
-
-print(re.search(r'l(l)', "hello").group(0))
