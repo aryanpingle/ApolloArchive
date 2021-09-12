58085c384e3b06a4e4fd3f74d09ab59a87e41ede
robo-mop <realaryanpingle@gmail.com>
2021-09-12 18:35:22 +0530

    WTF did I finish the datapoints page? :flushed:

--- JS/Datapoints.js
+++ JS/Datapoints.js
@@ -5,19 +5,43 @@ var datapoints = []
 
 var current_datatype = ""
 
+var POPUP_MODE = false
+
 var SIDEBAR = {
     title: 0,
     description: 0,
     image: 0
 }
 
+var POPUP = {
+    element: null,
+    header: null,
+    dtype: null,
+    audio: null,
+    text: null
+}
+
 async function setup_local() {
     print("Starting local setup")
+    
+    // Set up the SIDEBAR constant
     SIDEBAR = {}
     let [TITLE, DESCRIPTION, IMAGE] = gid("desktop-sidebar").children
     SIDEBAR.title = TITLE
     SIDEBAR.description = DESCRIPTION
     SIDEBAR.image = IMAGE
+    
+    // Set up the POPUP constant
+    let POPUP_ELEMENT = gid("datapoint-popup")
+    let [header, dtype, audio, text] = gid("popup-info").children
+    POPUP = {
+        element: POPUP_ELEMENT,
+        header: header,
+        dtype: dtype,
+        audio: audio,
+        text: text
+    }
+    
     setup_datatype_links() // Verified
     setup_close_buttons() // Verified
     setup_datapoint_numbers() // Verified
@@ -82,6 +106,12 @@ function setup_close_buttons() {
             datatype_section.classList.remove("shown")
             gid("datatype-container").classList.remove("shown")
             gid("datapoint-types").classList.add("shown")
+            // Stop playing audio, if at all
+            if(focused_datapoint && focused_datapoint.getAttribute("media-type")) {
+                let cum = focused_datapoint.querySelector("audio")
+                cum.pause()
+                cum.currentTime = 0
+            }
         }
     }
 }
@@ -91,6 +121,7 @@ function setup_close_buttons() {
 */
 
 function setup_datapoint_numbers() {
+    // All the datapoints
     [...document.getElementsByClassName("datapoint-container")].forEach(datapoint_container=>{
         [...datapoint_container.children].forEach((datapoint, index)=>{
             datapoint.setAttribute("position", index+1)
@@ -98,7 +129,8 @@ function setup_datapoint_numbers() {
             let media_type = datapoint.getAttribute("media-type")
             if(media_type) {
                 // Prep the play-button
-                let datapoint_audio = datapoint.lastElementChild.firstElementChild
+                let datapoint_audio = datapoint.querySelector(".datapoint-audio")
+                datapoint_audio.onclick = event=>event.stopPropagation()
                 let play_button = datapoint_audio.firstElementChild
                 play_button.onclick = event=>{
                     event.stopPropagation()
@@ -117,6 +149,33 @@ function setup_datapoint_numbers() {
             }
         })
     })
+    // Just the popup element
+    let datapoint = gid("popup-content")
+    let datapoint_audio = datapoint.querySelector(".datapoint-audio")
+    let play_button = datapoint_audio.children[1]
+    // print(play_button)
+    play_button.onclick = event=>{
+        event.stopPropagation()
+        toggle_audio_playback(play_button.previousElementSibling)
+    }
+    let audio = datapoint.querySelector("audio")
+    audio.addEventListener("timeupdate", function yuh() {
+        update_progress_bar(this)
+    })
+    audio.onended = event=>{
+        audio.parentElement.classList.remove("playing")
+    }
+}
+
+function close_popup() {
+    POPUP_MODE = false
+    POPUP.element.classList.remove("shown")
+    // Pause if playing
+    if(gid("popup-content").getAttribute("media-type")) {
+        POPUP.audio.classList.remove("playing")
+        POPUP.audio.firstElementChild.pause()
+        POPUP.audio.firstElementChild.currentTime = 0
+    }
 }
 
 /**
@@ -157,14 +216,34 @@ function set_datapoint_events() {
         
         // 3. Set onclick event for datapoints
         datapoint.onclick = event=>{
-            select_datapoint()
+            if(is_classic_layout()) {
+                init_popup(datapoint)
+            }
+            else select_datapoint()
         }
     }
 }
 
+function init_popup(datapoint) {
+    POPUP_MODE = true
+    // Due to the layout of datapoints and my shit naming system, #popup-content acts like the datapoint div, so add the media-type to it
+    let mt = datapoint.getAttribute("media-type")
+    if(mt) gid("popup-content").setAttribute("media-type", mt)
+    // Set the header
+    POPUP.header.innerHTML = datapoint.firstElementChild.firstElementChild.innerHTML
+    // Set the datatype
+    POPUP.dtype.innerHTML = current_datatype
+    // Set the audio
+    if(mt) POPUP.audio.firstElementChild.src = datapoint.getElementsByTagName("audio")[0].src
+    // Set the text
+    POPUP.text.innerHTML = datapoint.lastElementChild.lastElementChild.innerHTML
+    
+    POPUP.element.classList.add("shown")
+}
+
 function focus_datapoint(datapoint) {
     if(focused_datapoint == datapoint) return;
-    print("Changing focused_datapoint")
+    // print("Changing focused_datapoint")
     if(focused_datapoint) {
         focused_datapoint.classList.remove("focused")
     }
@@ -178,7 +257,7 @@ function focus_datapoint(datapoint) {
 function select_datapoint() {
     let current_selected = document.querySelector(".datapoint.selected")
     if(current_selected == focused_datapoint) {
-        print("Removing selection")
+        // print("Removing selection")
         current_selected.classList.remove("selected")
         if(current_selected.getAttribute("media-type")) {
             current_selected.getElementsByTagName("audio")[0].pause()
@@ -191,7 +270,7 @@ function select_datapoint() {
         if(current_selected.getAttribute("media-type")) current_selected.getElementsByTagName("audio")[0].pause()
         current_selected.classList.remove("selected")
     }
-    print("Selecting focused_datapoint")
+    // print("Selecting focused_datapoint")
     focused_datapoint.classList.add("selected")
 }
 
@@ -222,7 +301,7 @@ function prepare_datapoint_expansions() {
     for(const ele of document.getElementsByClassName("datapoint")) {
         ele.onmouseover = event=>{
             if(!is_classic_layout() || ele.classList.contains("selected")) return;
-            print("MOUSEOVERED")
+            // print("MOUSEOVERED")
             document.querySelector(".datapoint.selected").classList.remove("selected")
             ele.classList.add("selected");
             new Audio("click2.mp3").play()
@@ -239,6 +318,18 @@ function prepare_datapoint_expansions() {
 function setup_key_presses() {
     document.body.onkeydown = event=>{
         let key = event.key
+
+        // If popup is enabled
+        if(POPUP_MODE) {
+            if(key == "f") {
+                toggle_audio_playback(POPUP.audio.firstElementChild)
+            }
+            else if(key == "Escape") {
+                close_popup()
+            }
+            return;
+        }
+
         if (key == "q") {
             go_to_previous_page()
         }
@@ -251,7 +342,8 @@ function setup_key_presses() {
             toggle_layout()
         }
         else if(key == "f") {
-            select_datapoint()
+            if(is_classic_layout()) init_popup(focused_datapoint)
+            else select_datapoint()
         }
         else if(is_classic_layout()) {
             // Since this is a classic layout, we need to traverse the grid
@@ -273,7 +365,7 @@ function setup_key_presses() {
             else {
                 return
             }
-            print(`New Index: ${new_index}`)
+            // print(`New Index: ${new_index}`)
             focus_datapoint(datapoints[Math.max(1, Math.min(datapoints.length, new_index))-1])
         }
         // At this point it definitely isn't classic layout, so assume this is all for list layout
@@ -300,7 +392,7 @@ function setup_key_presses() {
 }
 
 function toggle_layout() {
-    print("Tab pressed")
+    // print("Tab pressed")
     
     if (document.body.classList.contains("classic-layout")) {
         // Switch to list layout
@@ -310,7 +402,7 @@ function toggle_layout() {
         document.body.classList.add("classic-layout")
     }
     
-    // Neccessary because display: none causes the first time to not work
+    // Necessary because display: none causes the first time to not work
     // set_datapoint_expansion_sizes()
 }
 
